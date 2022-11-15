d2d947903686b840f6eb5bae34d40a69f83b9c78
woo4826 <woo4826@gmail.com>
2022-11-15 17:07:52 +0900

    돌아가게 수정

--- lib/route/routes.dart
+++ lib/route/routes.dart
@@ -1,3 +1,4 @@
+import 'package:pet_app/home/home_view_page.dart';
 import 'package:pet_app/splash/splash_page.dart';
 import 'package:get/get.dart';
 
@@ -5,6 +6,7 @@ class GetXRouter {
   static final route = [
     // GetPage(name: MoreHelp.url, page: () => const MoreHelp()),
     GetPage(name: SplashPage.url, page: () => const SplashPage()),
+    GetPage(name: HomeViewPage.url, page: () => const HomeViewPage()),
     // GetPage(name: OnlineViewPage2.url, page: () => OnlineViewPage2()),
   ];
 }
