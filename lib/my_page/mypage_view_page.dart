8d248a03ae2516bbb2d87cc58a4df16679e3da3c
woo4826 <woo4826@gmail.com>
2022-11-28 20:33:52 +0900

    [Publishing] mypage depth1 complete

--- lib/my_page/mypage_view_page.dart
+++ lib/my_page/mypage_view_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:pet_app/common/common.dart';
 import 'package:get/get.dart';
 import 'package:pet_app/common/image_loader.dart';
@@ -24,89 +25,117 @@ class MyPageViewPage extends StatelessWidget {
                 ),
               ),
             ),
-            body: Column(
-              children: [
-                SizedBox(height: 20.r),
-                IntrinsicHeight(
-                  child: Stack(
-                    children: [
-                      Container(
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(5.r),
-                          color: CommonColor.gray06,
-                          boxShadow: [
-                            BoxShadow(
-                              color: Colors.grey.withOpacity(0.7),
-                              spreadRadius: 0,
-                              blurRadius: 5.0,
-                              offset: const Offset(0, 5), // changes position of shadow),
-                            )
-                          ],
-                        ),
-                        padding: EdgeInsets.symmetric(horizontal: 16.r),
-                        margin: EdgeInsets.symmetric(horizontal: 28.r),
-                        child: Column(
-                          children: [
-                            Padding(
-                              padding: EdgeInsets.only(top: 8.0.r, bottom: 15.r),
-                              child: Row(
-                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                children: [
-                                  Text(
-                                    "건국대학교",
-                                    style: CommonTextStyle(color: CommonColor.white, fontSize: 12),
-                                  ),
-                                  ClipRRect(
-                                    borderRadius: BorderRadius.circular(5.r),
-                                    child: Material(
-                                      color: const Color.fromRGBO(0, 0, 0, 1).withOpacity(0.6),
-                                      child: InkWell(
-                                        onTap: () {},
-                                        child: Container(
-                                          decoration: BoxDecoration(borderRadius: BorderRadius.circular(15.r)),
-                                          padding: EdgeInsets.symmetric(horizontal: 12.r, vertical: 5.r),
-                                          child: Row(
-                                            children: [
-                                              Image.asset(
-                                                "assets/images/edit_pencil.png",
-                                                width: 8.r,
-                                                height: 8.r,
-                                              ),
-                                              SizedBox(width: 5.r),
-                                              Text(
-                                                "수정",
-                                                style: CommonTextStyle(
-                                                  color: CommonColor.white,
-                                                  fontSize: 10,
-                                                  fontWeight: FontWeight.w500,
+            body: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 28.r),
+              child: Column(
+                children: [
+                  SizedBox(height: 20.r),
+                  IntrinsicHeight(
+                    child: Stack(
+                      children: [
+                        Container(
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(5.r),
+                            color: CommonColor.gray06,
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.grey.withOpacity(0.7),
+                                spreadRadius: 0,
+                                blurRadius: 5.0,
+                                offset: const Offset(0, 5), // changes position of shadow),
+                              )
+                            ],
+                          ),
+                          padding: EdgeInsets.symmetric(horizontal: 16.r),
+                          child: Column(
+                            children: [
+                              Padding(
+                                padding: EdgeInsets.only(top: 8.0.r, bottom: 15.r),
+                                child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Text(
+                                      "건국대학교",
+                                      style: CommonTextStyle(color: CommonColor.white, fontSize: 12),
+                                    ),
+                                    ClipRRect(
+                                      borderRadius: BorderRadius.circular(5.r),
+                                      child: Material(
+                                        color: const Color.fromRGBO(0, 0, 0, 1).withOpacity(0.6),
+                                        child: InkWell(
+                                          onTap: () {},
+                                          child: Container(
+                                            decoration: BoxDecoration(borderRadius: BorderRadius.circular(15.r)),
+                                            padding: EdgeInsets.symmetric(horizontal: 12.r, vertical: 5.r),
+                                            child: Row(
+                                              children: [
+                                                Image.asset(
+                                                  "assets/images/edit_pencil.png",
+                                                  width: 8.r,
+                                                  height: 8.r,
                                                 ),
-                                              ),
-                                            ],
+                                                SizedBox(width: 5.r),
+                                                Text(
+                                                  "수정",
+                                                  style: CommonTextStyle(
+                                                    color: CommonColor.white,
+                                                    fontSize: 10,
+                                                    fontWeight: FontWeight.w500,
+                                                  ),
+                                                ),
+                                              ],
+                                            ),
                                           ),
                                         ),
                                       ),
-                                    ),
+                                    )
+                                  ],
+                                ),
+                              ),
+                              Row(
+                                children: [
+                                  ImageLoader(
+                                    url: controller.user.value?.profileImageUrl ?? "",
+                                    height: 50.r,
+                                    width: 50.r,
+                                  ),
+                                  SizedBox(width: 10.r),
+                                  Column(
+                                    crossAxisAlignment: CrossAxisAlignment.start,
+                                    children: [
+                                      Row(
+                                        crossAxisAlignment: CrossAxisAlignment.end,
+                                        children: [
+                                          Text(
+                                            controller.user.value?.username ?? "",
+                                            style: CommonTextStyle(fontSize: 18.r, color: CommonColor.white),
+                                          ),
+                                        ],
+                                      ),
+                                      Row(
+                                        crossAxisAlignment: CrossAxisAlignment.end,
+                                        children: [
+                                          Text(
+                                            "${controller.user.value?.age ?? 1}살",
+                                            style: CommonTextStyle(fontSize: 12.r, color: CommonColor.gray03),
+                                          ),
+                                        ],
+                                      ),
+                                    ],
                                   )
                                 ],
                               ),
-                            ),
-                            Row(
-                              children: [
-                                ImageLoader(
-                                  url: controller.user.value?.profileImageUrl ?? "",
-                                  height: 50.r,
-                                  width: 50.r,
-                                ),
-                                SizedBox(width: 10.r),
-                                Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
+                              SizedBox(height: 60.r),
+                              Padding(
+                                padding: EdgeInsets.only(bottom: 21.r),
+                                child: Column(
                                   children: [
                                     Row(
                                       crossAxisAlignment: CrossAxisAlignment.end,
                                       children: [
                                         Text(
-                                          controller.user.value?.username ?? "",
-                                          style: CommonTextStyle(fontSize: 18.r, color: CommonColor.white),
+                                          controller.user.value?.upperMajor ?? "",
+                                          style: CommonTextStyle(fontSize: 12.r, color: CommonColor.white),
                                         ),
                                       ],
                                     ),
@@ -114,65 +143,77 @@ class MyPageViewPage extends StatelessWidget {
                                       crossAxisAlignment: CrossAxisAlignment.end,
                                       children: [
                                         Text(
-                                          "${controller.user.value?.age ?? 1}살",
-                                          style: CommonTextStyle(fontSize: 12.r, color: CommonColor.gray03),
+                                          controller.user.value?.major ?? "",
+                                          style: CommonTextStyle(fontSize: 12.r, color: CommonColor.white),
+                                        ),
+                                      ],
+                                    ),
+                                    Row(
+                                      crossAxisAlignment: CrossAxisAlignment.end,
+                                      children: [
+                                        Text(
+                                          controller.user.value?.major ?? "",
+                                          style: CommonTextStyle(fontSize: 12.r, color: CommonColor.white),
                                         ),
                                       ],
                                     ),
                                   ],
-                                )
-                              ],
-                            ),
-                            SizedBox(height: 60.r),
-                            Padding(
-                              padding: EdgeInsets.only(bottom: 21.r),
-                              child: Column(
-                                children: [
-                                  Row(
-                                    crossAxisAlignment: CrossAxisAlignment.end,
-                                    children: [
-                                      Text(
-                                        controller.user.value?.upperMajor ?? "",
-                                        style: CommonTextStyle(fontSize: 12.r, color: CommonColor.white),
-                                      ),
-                                    ],
-                                  ),
-                                  Row(
-                                    crossAxisAlignment: CrossAxisAlignment.end,
-                                    children: [
-                                      Text(
-                                        controller.user.value?.major ?? "",
-                                        style: CommonTextStyle(fontSize: 12.r, color: CommonColor.white),
-                                      ),
-                                    ],
-                                  ),
-                                  Row(
-                                    crossAxisAlignment: CrossAxisAlignment.end,
-                                    children: [
-                                      Text(
-                                        controller.user.value?.major ?? "",
-                                        style: CommonTextStyle(fontSize: 12.r, color: CommonColor.white),
-                                      ),
-                                    ],
-                                  ),
-                                ],
-                              ),
-                            )
-                          ],
+                                ),
+                              )
+                            ],
+                          ),
                         ),
-                      ),
-                      Positioned(
-                        bottom: 0,
-                        right: 0,
-                        child: Image.asset(
-                          "assets/images/konkuk_logo.png",
-                          width: 170.r,
+                        Positioned(
+                          bottom: 0,
+                          right: 0,
+                          child: Image.asset(
+                            "assets/images/konkuk_logo.png",
+                            width: 170.r,
+                          ),
                         ),
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
-                ),
-              ],
+                  SizedBox(height: 30.r),
+                  checkbox("내 소개 수정하기", context),
+                  checkbox("내가 원하는 룸메 수정하기", context),
+                ],
+              ),
             )));
+  }
+
+  checkbox(String header, BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(bottom: 15.r),
+      child: InkWell(
+        splashColor: Colors.black.withOpacity(0.05),
+        onTap: () {
+          // routeFunction(header, context, btnIndex, body);
+        },
+        child: Container(
+          decoration: BoxDecoration(color: CommonColor.disabledGrey, borderRadius: BorderRadius.circular(5.r)),
+          padding: EdgeInsets.symmetric(horizontal: 20.r, vertical: 16.r),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                header,
+                style: CommonTextStyle(
+                  color: CommonColor.black,
+                  fontSize: 16,
+                ),
+              ).c,
+              const Spacer(),
+              GestureDetector(
+                  onTap: () {
+                    // routeFunction(header, context, btnIndex, body);
+                  },
+                  child: SvgPicture.asset('assets/icons/right_arrow.svg'))
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
