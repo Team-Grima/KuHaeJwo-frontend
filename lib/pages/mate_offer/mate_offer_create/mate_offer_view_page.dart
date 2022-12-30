de7f231df448f7548127870c9958de4a0228a8a0
태승 <taeseung@taeseung-ui-MacBookPro.local>
2022-12-30 17:56:49 +0900

    user detail 마무리

--- lib/pages/mate_offer/mate_offer_create/mate_offer_view_page.dart
+++ lib/pages/mate_offer/mate_offer_create/mate_offer_view_page.dart
@@ -18,7 +18,9 @@ class MateOfferViewPage extends StatelessWidget {
     return SafeArea(
       child: Scaffold(
         backgroundColor: CommonColor.white,
-        appBar: CommonAppBar(context: context, title: controller.authService.myMateOffer.value?.title ?? "제목이 없어요",
+        appBar: CommonAppBar(
+            context: context,
+            title: controller.authService.myMateOffer.value?.title ?? "제목이 없어요",
             // title: "내 쿠해줘 게시글 미리보기",
             actions: [
               Container(
@@ -28,7 +30,8 @@ class MateOfferViewPage extends StatelessWidget {
                     Get.toNamed(MateOfferEditViewPage.url);
                   },
                   child: Padding(
-                    padding: EdgeInsets.symmetric(vertical: 18.r, horizontal: 12.r),
+                    padding:
+                        EdgeInsets.symmetric(vertical: 18.r, horizontal: 12.r),
                     child: SvgPicture.asset(
                       "assets/icons/edit_mate_offer_button.svg",
                       color: CommonColor.black,
@@ -41,24 +44,32 @@ class MateOfferViewPage extends StatelessWidget {
           children: [
             SingleChildScrollView(
               child: Obx(
-                () => controller.authService.userPrefer.value != null && controller.authService.myMateOffer.value != null
+                () => controller.authService.userPrefer.value != null &&
+                        controller.authService.myMateOffer.value != null
                     ? IntrinsicHeight(
                         child: Column(
                         children: [
                           Padding(
                             // padding: EdgeInsets.only(top: 16.r, left: 24.r, right: 24.r),
-                            padding: EdgeInsets.symmetric(vertical: 12.r, horizontal: 24.r),
+                            padding: EdgeInsets.symmetric(
+                                vertical: 12.r, horizontal: 24.r),
                             child: Column(
                               crossAxisAlignment: CrossAxisAlignment.start,
                               children: [
                                 userProfileFragment(controller),
-                                mateOfferBodyFragment(controller.authService.myMateOffer.value!.body),
-                                userDetailInfoFragment(controller.authService.userInfoDetail.value!),
-                                userPreferFragment(controller.authService.userPrefer.value!.preferList),
+                                mateOfferBodyFragment(controller
+                                    .authService.myMateOffer.value!.body),
+                                userDetailInfoFragment(controller
+                                    .authService.userInfoDetail.value!),
+                                userPreferFragment(controller
+                                    .authService.userPrefer.value!.preferList),
                                 Text(
-                                  controller.authService.myMateOffer.value!.body ?? "",
+                                  controller.authService.myMateOffer.value!
+                                          .body ??
+                                      "",
                                   overflow: TextOverflow.visible,
-                                  style: CommonTextStyle(fontSize: 14, color: CommonColor.gray03),
+                                  style: CommonTextStyle(
+                                      fontSize: 14, color: CommonColor.gray03),
                                 ),
                               ],
                             ),
@@ -79,7 +90,8 @@ class MateOfferViewPage extends StatelessWidget {
       children: [
         ImageLoader(
           // url: userDetailData.profileImageUrl ?? "",
-          url: "https://enter.kku.ac.kr/mbshome/mbs/wwwkr/renewal/images/identity/ui_am.png",
+          url:
+              "https://enter.kku.ac.kr/mbshome/mbs/wwwkr/renewal/images/identity/ui_am.png",
           height: 38.r,
           width: 38.r,
         ),
@@ -104,7 +116,8 @@ class MateOfferViewPage extends StatelessWidget {
                 Text(
                   // post.time ?? "" == ,
                   "방금",
-                  style: CommonTextStyle(fontSize: 10, color: CommonColor.disabledGrey),
+                  style: CommonTextStyle(
+                      fontSize: 10, color: CommonColor.disabledGrey),
                 ),
               ],
             ),
@@ -138,62 +151,65 @@ class MateOfferViewPage extends StatelessWidget {
           style: CommonTextStyle(fontSize: 14, color: CommonColor.black),
         ),
         Text(
-          generateDetailStirng(userInfoDetailResponse),
+          generateDetailString(userInfoDetailResponse),
           style: CommonTextStyle(fontSize: 14, color: CommonColor.black),
         ),
       ],
     );
   }
 
-  generateDetailStirng(UserInfoDetailResponse detail) {
+  generateDetailString(UserInfoDetailResponse detail) {
     String res = "";
-    res += "✅";
+    res += "✅ ";
     res += detail.cleanHabit == "LAZY" ? "몰아서 치워요" : "바로 바로 치워요";
-    res += "\n✅";
+    res += "\n✅ ";
     res += detail.washingTime == "WAKE_UP"
         ? "보통은 일어나서 씻어요"
         : detail.washingTime == "BEFORE_SLEEP"
             ? "보통은 자기 전에 씻고 자요"
             : "아침 저녁으로 여러번 씻어요";
-    res += "\n✅";
+    res += "\n✅ ";
     res += detail.alcohol == "YES"
         ? "술을 마셔요"
         : detail.alcohol == "OFTEN"
             ? "술을 자주 마셔요"
             : "술을 마시지 않아요";
-    res += "\n✅";
+    res += "\n✅ ";
     res += detail.smoking == "YES" ? "담배를 피지 않아요 " : "흡연자에요";
-    res += "\n✅";
+    res += "\n✅ ";
     res += detail.sleepingTime == "BEFORE_12PM"
         ? "밤 열두시 전에 자요"
         : detail.sleepingTime == "AFTER_12PM"
             ? "밤 열두시 넘어서 자요"
             : "해뜨고나서.. 자요";
-    res += "\n✅";
+    res += "\n✅ ";
     res += detail.sleepingHabit == "SNORE"
         ? "코를 조금 골아요"
         : detail.sleepingHabit == "BRUXISM"
             ? "이를 좀 갈아요"
             : "잠버릇이 없어요";
-    res += "\n✅";
+    res += "\n✅ ";
     res += detail.sleeper == "LIGHT" ? "잠귀가 어두워서 잘 안깨요" : "잠귀가 밝아서 잘 깨요";
-    res += "\n✅";
+    res += "\n✅ ";
     res += detail.wakeUpTime == "BEFORE_AM09"
         ? "기상은 아침 9시 쯤 일어나요"
         : detail.wakeUpTime == "AROUND_AM12"
             ? "아침 12시 쯤 일어나요"
             : "오후에 일어나요";
-    res += "\n✅";
-    res += detail.alarm == "" ? "" : "";
-    res += "\n✅";
-    res += detail.outing == "" ? "" : "";
-    res += "\n✅";
-    res += detail.bug == "" ? "" : "";
-    res += "\n✅";
-    res += detail.temperature == "" ? "" : "";
-    res += "\n✅";
-    res += detail.friend == "" ? "" : "";
-    res += "\n✅";
+    res += "\n✅ ";
+    res += detail.alarm == "YES" ? "알람은 잘 듣는 편이에요" : "알람은 잘 못 듣는 편이에요";
+    res += "\n✅ ";
+    res += detail.outing == "MORE" ? "밖에 있는 시간이 많아요" : "집에 있는 시간이 많아요";
+    res += "\n✅ ";
+    res += detail.bug == "YES" ? "벌레를 잘 잡아요" : "벌레는 무서워서 못 잡아요";
+    res += "\n✅ ";
+    res += detail.temperature == "COLD"
+        ? "추위를 많이 타요"
+        : detail.temperature == "HEAT"
+            ? "더위를 많이 타요"
+            : "추운거 더운거 상관없어요";
+    res += "\n✅ ";
+    res += detail.friend == "BEST_FRIEND" ? "룸메와 베프가 되고 싶어요" : "룸메랑은 거리가 필요해요";
     return res;
   }
 
@@ -225,10 +241,12 @@ class MateOfferViewPage extends StatelessWidget {
                               //   controller.addSelectedPrefer(item);
                               // },
                               child: Container(
-                                padding: EdgeInsets.symmetric(horizontal: 6.r, vertical: 4.r),
+                                padding: EdgeInsets.symmetric(
+                                    horizontal: 6.r, vertical: 4.r),
                                 child: Text(
                                   "# $item",
-                                  style: CommonTextStyle(fontSize: 13, color: Colors.black),
+                                  style: CommonTextStyle(
+                                      fontSize: 13, color: Colors.black),
                                 ).c,
                               ),
                             ),
